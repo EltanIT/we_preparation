998a05c19be711354afeaba258e40656a53ec1b0
gera <georgemichkasiv@gmail.com>
2024-02-28 11:57:51 +0500

    REFACTOR

--- app/src/androidTest/java/com/example/ws_preparation/Tests.kt
+++ app/src/androidTest/java/com/example/ws_preparation/Tests.kt
@@ -1,10 +1,9 @@
 package com.example.ws_preparation
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.example.ws_preparation.data.manger.QueueMangerImpl
 import com.example.ws_preparation.domain.entities.OnboardPage
-import com.example.ws_preparation.domain.manger.QueueManger
 import com.example.ws_preparation.domain.useCases.onboard.AddItemInQueue
 import com.example.ws_preparation.domain.useCases.onboard.ClearQueue
 import com.example.ws_preparation.domain.useCases.onboard.CreateDefaultQueue
@@ -15,16 +14,13 @@ import com.example.ws_preparation.domain.useCases.onboard.OnboardUseCases
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-import org.junit.Before
 
-
-
-//GREEN
+//REFACTOR
 @RunWith(AndroidJUnit4::class)
 class Tests {
 
@@ -45,50 +41,56 @@ class Tests {
         )
     }
     @Test
-    fun test() {
+    fun titleAndImageFromQueueGetCorrect() {
         coroutineScope.launch {
             onboardUseCases.clearQueue()
             onboardUseCases.addItemInQueue(OnboardPage("title1", "description1", 0))
             onboardUseCases.addItemInQueue(OnboardPage("title2", "description2", 1))
             assertEquals(onboardUseCases.getItemFromQueue().title, "title1")
             assertEquals(onboardUseCases.getItemFromQueue().title, "title2")
+        }
+    }
 
 
-
-
-
+    @Test
+    fun correctGetItemFromQueue() {
+        coroutineScope.launch {
             onboardUseCases.clearQueue()
             val sizeBeforeAddItemInQueue = onboardUseCases.isQueueIsEmpty()
             onboardUseCases.addItemInQueue(OnboardPage("title", "description", 0))
             val sizeAfterAddItemInQueue = onboardUseCases.isQueueIsEmpty()
             assertTrue(sizeBeforeAddItemInQueue == sizeAfterAddItemInQueue)
-
-
-
-
-
+        }
+    }
+    @Test
+    fun getCorrectButtonStateWithFullQueue() {
+        coroutineScope.launch {
             onboardUseCases.clearQueue()
             onboardUseCases.addItemInQueue(OnboardPage("title1", "description1", 0))
             onboardUseCases.addItemInQueue(OnboardPage("title2", "description2", 1))
             assertEquals(onboardUseCases.getButtonStateQueue(), "Next")
+        }
+    }
 
-
-
-
+    @Test
+    fun getCorrectButtonStateWithEmptyQueue() {
+        coroutineScope.launch {
             onboardUseCases.clearQueue()
             assertEquals(onboardUseCases.getButtonStateQueue(), "Sign Up")
+        }
+    }
 
-
-
-
+    @Test
+    fun getCorrectSignInNavigation() {
+        coroutineScope.launch {
             onboardUseCases.clearQueue()
+            //переход произошел
             assertTrue(onboardUseCases.isQueueIsEmpty() == true)
 
             onboardUseCases.addItemInQueue(OnboardPage("title1", "description1", 0))
+            //переход отсутствует
             assertFalse(onboardUseCases.isQueueIsEmpty() == true)
-
-
-
         }
     }
+
 }
